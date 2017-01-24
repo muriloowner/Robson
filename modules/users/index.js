115a539a9007dd33cc0c89bfc15c60622e9ab62a
Guilherme Catini <guilherme@catini.org>
2017-01-24 20:03:13 -0200

    ae caralho

--- modules/users/index.js
+++ modules/users/index.js
@@ -19,8 +19,45 @@ const userModel = mongoose.model('usuario', userSchema)
 router.get('/', (req, res) => {
   const query = {}
   userModel.find(query, (err, data) => {
-    if (err) return res.json(data)
+    if (err) return res.json(err)
     return res.render('./users/list', { data: data })
+  })
+})
+
+router.get('/form', (req, res) => {
+  res.render('./users/form', { data: {} })
+})
+
+router.get('/form/:id', (req, res) => {
+  const query = { _id: req.params.id }
+  userModel.findOne(query, (err, data) => {
+    if (err) return res.json(err)
+    res.render('./users/form', { data: data })
+  })
+})
+
+router.post('/form', (req, res) => {
+  const body = req.body
+  const query = { _id: req.body.id }
+  const id = req.body.id
+  if (id) {
+    userModel.update(query, body, (err, data) => {
+      if (err) return res.json(data)
+      res.redirect('/users/')
+    })
+  } else {
+    userModel.create(body, (err, data) => {
+      if (err) return res.json(err)
+      res.redirect('/users/')
+    })
+  }
+})
+
+router.get('/remove/:id', (req, res) => {
+  const query = { _id: req.params.id }
+  userModel.remove(query, (err, data) => {
+    if (err) return res.json(err)
+    res.redirect('/users/')
   })
 })
 
